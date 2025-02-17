6d57c9ff92f10053e91358b8a20161b348ab38b5
João Victor Pequeno Damasceno <joaodamasceno0502@gmail.com>
2025-02-17 17:50:51 -0300

    Delete Lista 2 directory

--- Lista 2/Questao16.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(){
-    int x=0;
-    printf("Digite o valor:\n");
-    scanf("%d", &x);
-    printf("Valor absoluto desse número: %d", abs(x));
-    return 0;
-}
--- Lista 2/Questao19.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int x=0;
-    printf("Digite o número:\n");
-    scanf("%d", &x);
-    (x%2==0)?printf("O número é par"):printf("O número é ímpar");
-    return 0;
-}
